757f20516efd1e04ce95e0f863888e004e78a553
suzannelv <lvmeijuan9011@gmail.com>
2023-10-14 20:02:24 +0200

    logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,7 +66,7 @@ const config = {
             // Replace with your project's social card
             image: "img/docusaurus-social-card.jpg",
             navbar: {
-                title: "PHP 課程 (中文版)",
+                title: " - PHP 課程 (中文版)",
                 logo: {
                     alt: "My Site Logo",
                     src: "img/logo.svg",
